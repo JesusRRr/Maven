aa033f358e67edea2e8fb60b56af5fdef107bd2f
JesusRRr <rolonjesus1996@gmail.com>
2020-06-17 15:45:43 -0500

    feature: conexion with DB successfully

--- src/main/java/com/rolon/Demo.java
+++ src/main/java/com/rolon/Demo.java
@@ -1,16 +1,27 @@
 package com.rolon;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import com.rolon.db.DbConnector;
 
 public class Demo {
+	private DbConnector DB;
 	
 	public static void main(String[] args) {
 		try {
-			DbConnector.connection();
+			DbConnector DB = new DbConnector();
+			DB.connection();
+			
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
+		
 	}
 }
--- src/main/java/com/rolon/db/DbConnector.java
+++ src/main/java/com/rolon/db/DbConnector.java
@@ -3,20 +3,34 @@ package com.rolon.db;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DbConnector {
-	private static Properties properties;
-	private static FileInputStream file;
+	private  Properties properties;
+	private  FileInputStream file;
+	private  Connection connection;
+	private  String url;
+	private  String user;
+	private  String password;
 	
-	public static void connection() throws IOException {
+	public DbConnector() throws IOException {
 		getConfigurationFile();
 		properties=new Properties();
 		properties.load(file);
-		System.out.println(properties.getProperty("url"));
+		url=properties.getProperty("url");
+		user=properties.getProperty("user");
+		password=properties.getProperty("password");
 	}
 	
-	public static void getConfigurationFile() {
+	public void connection() throws SQLException {
+	
+		connection= DriverManager.getConnection(url, user, password);
+	}
+	
+	private void getConfigurationFile() {
 		try {
 			file = new FileInputStream("./config.properties");
 		} catch (FileNotFoundException e) {
